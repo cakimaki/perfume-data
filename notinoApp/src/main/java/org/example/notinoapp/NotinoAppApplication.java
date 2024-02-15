1c11e904cce22e5f4c60b31326e9882b0afa8c0f
cakimaki <dani.tsakovski3@gmail.com>
2024-02-15 16:09:39 +0200

    springboot

---
+++ notinoApp/src/main/java/org/example/notinoapp/NotinoAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.notinoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotinoAppApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(NotinoAppApplication.class, args);
+	}
+	
+}
---
+++ notinoApp/src/test/java/org/example/notinoapp/NotinoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.notinoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotinoAppApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
